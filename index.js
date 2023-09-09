b243db9a19702e46d0988dc7cbc180f9f63c3947
Marisa <marisavandellos@gmail.com>
2023-09-09 10:31:21 -0700

    organized code and finished 2 parts of first class functions

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 //First-class Function
 //Each problem statement is one function
-//Assign a function to a variable in JavaScript and use it to return another function that performs a specific operation, 
-//such as multiplying two numbers or transforming a string.
-//2nd is a partial application
+//Assign a function to a variable in JavaScript and use it to return another function that performs a specific operation
 
-// const umbrellaFunction = (num) => { //1st function
-//     return func(a) * number;
-// }
+//Assign a function to a variable and use it to convert a string to uppercase
+const testString = "Welcome";
+const upperCaseFunction = () => {
+    return (str) => {
+         return str.toUpperCase();
+    }
+}
+const functionExecute = upperCaseFunction();
+console.log(functionExecute(testString));
 
-// let result = umbrellaFunction(arg);
+//Create a function that takes a number as input and returns another function that multiplies its input with the original number
+//partial application
+const multiplyNumbers = (num1) => {
+    return (num2) => {
+        return num1 * num2;
+    }
+}
+const takeNum = multiplyNumbers(3);
+const multipliedNums = takeNum(7);
+console.log(multipliedNums);
 
 //Higher-order Function
 //function takes an array and a function as arguments
 //modify each element in the input and returns a new array containing the modified elements
 
+//Converts all string elements to lowercase
 const arrayOfStrings = ["Hippopotamus", "King Cobra", "Giant Panda", "Crocodile"]
 const convertToLowercase = (capitalString) => {
     return capitalString.toLowerCase();
